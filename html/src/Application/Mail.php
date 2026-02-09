f02dc58f859bc6c4b27c15ea9e92956b3a525433
Harnish Prajapati <harnishprajapati2906@gmail.com>
2026-02-09 16:59:46 -0500

    Update Mail.php
    Added some mail functions to get all mails, mail by id , update and delete mail.

--- html/src/Application/Mail.php
+++ html/src/Application/Mail.php
@@ -11,10 +11,47 @@ class Mail {
         $this->pdo = $pdo;
     }
 
+    // To create mail
     public function createMail($subject, $body) {
         $stmt = $this->pdo->prepare("INSERT INTO mail (subject, body) VALUES (?, ?) RETURNING id");
         $stmt->execute([$subject, $body]);
 
         return $stmt->fetchColumn();
+    }
+
+    // To review all mails
+    public function getAllmails() {
+        $sql = "SELECT * FROM mail ORDER BY id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+
+        $mail = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // To review a mail with id
+    public function getMailById($id) {
+        $sql = "SELECT * FROM mail WHERE id=?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$id]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // To update a mail by id
+    public function updateMail($id, $subject, $body) {
+        $sql = "UPDATE mail SET subject = ?, body = ? WHERE id = ?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$subject, $body, $id]);
+
+        $updatedMail = $this->getMailById($id);
+        return $updatedMail;
+    }
+
+     // To delete a mail
+    public function deleteMail($id) {
+        $sql = "DELETE FROM mail WHERE id=?";
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([$id]);
     }
 }
