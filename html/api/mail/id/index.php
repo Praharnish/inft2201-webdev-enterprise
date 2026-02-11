1ca9f9f48f12dd6aaf18e8f38277d39086e94604
Harnish Prajapati <harnishprajapati2906@gmail.com>
2026-02-11 18:50:00 -0500

    get, update and delete by id

--- html/api/mail/id/index.php
+++ html/api/mail/id/index.php
@@ -1,9 +1,52 @@
 <?php
-declare(strict_types=1);
+require '../../vendor/autoload.php';
 
-echo "<h1>SupportDesk Demo</h1>";
-echo "<p>PHP + Apache container is running. </p>";
-echo "<p>Next step:add REST endpoints + Composer + PHPUnit.</p>";
-echo "";
+use Application\Mail;
+use Application\Page;
 
-phpinfo();
+$dsn = "pgsql:host=" . getenv('DB_PROD_HOST') . ";dbname=" . getenv('DB_PROD_NAME');
+try {
+    $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo "Database connection failed: " . $e->getMessage();
+    exit;
+}
+
+$mail = new Mail($pdo);
+$page = new Page();
+
+// To get the url
+$uri = $_SERVER['REQUEST_URI'];
+$parts = explode('/' , trim($uri , '/'));
+$id = end($parts);
+
+// To get the method
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($uri === "/api/mail/$id") {
+    switch ($method) {
+        // To get the mail by id
+        case 'GET':
+            $page->item($mail->getMailById($id));
+            break;
+
+        // To update the mail
+        case 'PUT':
+            $json = file_get_contents("php://input");
+            $data = json_decode($json, true);
+            $page->item($mail->updateMail($data['subject'], $data['body']));
+            break;
+
+        // To gelet the mail by id
+        case 'DELETE':
+            $page->item($mail->deleteMail($id));
+            break;
+
+        // Default condition
+        default:
+            $page->notFound();
+    }
+};
